6302f3fc2ec8d0b4b2bc53c8ba0a66d377b008e2
Carlo Santos <carlox7@gmail.com>
2017-09-28 19:13:22 -0700

    started redux tutorial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'JavaScript: The Good Parts'},
+    {title: 'Goblet of Fire'},
+    {title: 'A Song of Ice and Fire'},
+    {title: 'Winds of Winter'},
+  ];
+}
