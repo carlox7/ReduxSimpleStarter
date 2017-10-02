11c29b594cf875ba5468ea0d3b0d43ba0b455bca
Carlo Santos <carlox7@gmail.com>
2017-10-02 15:14:58 -0700

    finished redux book tutorial

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
-function selectBook(book){
-  console.log('book has been selected:', book.title);
+export function selectBook(book){
+  //selectbook is an action creator, needs to return an action which is an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book,
+  };
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Goblet of Fire'},
-    {title: 'A Song of Ice and Fire'},
-    {title: 'Winds of Winter'},
+    {title: 'JavaScript: The Good Parts', pages: 101},
+    {title: 'Goblet of Fire', pages: 60},
+    {title: 'A Song of Ice and Fire', pages: 800},
+    {title: 'Winds of Winter', pages: 900},
   ];
 }
